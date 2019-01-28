fda74a7bdc5d55f849d6ec9b89bba39a63d2c142
Jarek Radosz <jradosz@gmail.com>
2019-01-28 12:00:31 +0100

    Add TypeScript definition

---
+++ index.d.ts
@@ -0,0 +1,93 @@
+interface CommonOptions {
+	/**
+	 * The issue body.
+	 */
+	body?: string;
+
+	/**
+	 * The issue title.
+	 */
+	title?: string;
+
+	/**
+	 * Use an [issue template](https://help.github.com/articles/manually-creating-a-single-issue-template-for-your-repository/).
+	 *
+	 * @example
+	 *
+	 * 'unicorn.md' // If you want to use a template at `ISSUE_TEMPLATE/unicorn.md`.
+	 */
+	template?: string;
+
+	/**
+	 * The labels for the issue.
+	 *
+	 * *Requires the user to have the permission to add labels.*
+	 */
+	labels?: string[];
+
+	/**
+	 * The milestone for the issue.
+	 *
+	 * *Requires the user to have the permission to add milestone.*
+	 */
+	milestone?: string;
+
+	/**
+	 * The user to assign to the issue.
+	 *
+	 * *Requires the user to have the permission to add assignee.*
+	 */
+	assignee?: string;
+
+	/**
+	 * The projects to add the issue to.
+	 * The project reference format is `user/<project-number>`, for example, if the URL to the project is `https://github.com/sindresorhus/some-repo/projects/3`, the project reference would be `some-repo/3`.
+	 *
+	 * *Requires the user to have the permission to add projects.*
+	 */
+	projects?: string[];
+}
+
+interface RepoUrlOptions extends CommonOptions {
+	/**
+	 * The full URL to the repo.
+	 */
+	repoUrl: string;
+}
+
+interface UserAndRepoOptions extends CommonOptions {
+	/**
+	 * GitHub username or organization.
+	 */
+	user: string;
+
+	/**
+	 * GitHub repo.
+	 */
+	repo: string;
+}
+
+/**
+ * You are required to either specify the `repoUrl` option or both the `user` and `repo` options.
+ */
+export type Options = RepoUrlOptions | UserAndRepoOptions
+
+/**
+ * Generate a URL for opening a new GitHub issue with prefilled title, body, and other fields.
+ *
+ * @example
+ *
+ * import newGithubIssueUrl from 'new-github-issue-url';
+ * import opn from 'opn';
+ *
+ * const url = newGithubIssueUrl({
+ * 	user: 'sindresorhus',
+ * 	repo: 'new-github-issue-url',
+ * 	body: '\n\n\n---\nI\'m a human. Please be nice.'
+ * });
+ * //=> 'https://github.com/sindresorhus/new-github-issue-url/issues/new?body=%0A%0A%0A---%0AI%27m+a+human.+Please+be+nice.'
+ *
+ * // Then open it
+ * opn(url);
+ */
+export default function newGithubIssueUrl(options: Options): string;
