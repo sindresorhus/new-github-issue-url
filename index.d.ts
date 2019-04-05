77b911a0db108279fe85971bdd4083d977b6fe8f
Dimitri Benin <dimitri.benin@gmail.com>
2019-04-06 01:48:51 +0200

    Refactor TypeScript definition to CommonJS compatible export

--- index.d.ts
+++ index.d.ts
@@ -1,93 +1,106 @@
-interface CommonOptions {
-	/**
-	 * The issue body.
-	 */
-	readonly body?: string;
+declare namespace newGithubIssueUrl {
+	interface CommonOptions {
+		/**
+		The issue body.
+		*/
+		readonly body?: string;
+
+		/**
+		The issue title.
+		*/
+		readonly title?: string;
+
+		/**
+		Use an [issue template](https://help.github.com/articles/manually-creating-a-single-issue-template-for-your-repository/).
+
+		@example
+		```
+		'unicorn.md' // If you want to use a template at `ISSUE_TEMPLATE/unicorn.md`.
+		```
+		*/
+		readonly template?: string;
+
+		/**
+		The labels for the issue.
+
+		_Requires the user to have the permission to add labels._
+		*/
+		readonly labels?: string[];
+
+		/**
+		The milestone for the issue.
+
+		_Requires the user to have the permission to add milestone._
+		*/
+		readonly milestone?: string;
+
+		/**
+		The user to assign to the issue.
+
+		_Requires the user to have the permission to add assignee._
+		*/
+		readonly assignee?: string;
+
+		/**
+		The projects to add the issue to.
+		The project reference format is `user/<project-number>`, for example, if the URL to the project is `https://github.com/sindresorhus/some-repo/projects/3`, the project reference would be `some-repo/3`.
+
+		_Requires the user to have the permission to add projects._
+		*/
+		readonly projects?: string[];
+	}
+
+	interface RepoUrlOptions extends CommonOptions {
+		/**
+		The full URL to the repo.
+		*/
+		readonly repoUrl: string;
+	}
+
+	interface UserAndRepoOptions extends CommonOptions {
+		/**
+		GitHub username or organization.
+		*/
+		readonly user: string;
+
+		/**
+		GitHub repo.
+		*/
+		readonly repo: string;
+	}
 
 	/**
-	 * The issue title.
-	 */
-	readonly title?: string;
-
-	/**
-	 * Use an [issue template](https://help.github.com/articles/manually-creating-a-single-issue-template-for-your-repository/).
-	 *
-	 * @example
-	 *
-	 * 'unicorn.md' // If you want to use a template at `ISSUE_TEMPLATE/unicorn.md`.
-	 */
-	readonly template?: string;
-
-	/**
-	 * The labels for the issue.
-	 *
-	 * *Requires the user to have the permission to add labels.*
-	 */
-	readonly labels?: string[];
-
-	/**
-	 * The milestone for the issue.
-	 *
-	 * *Requires the user to have the permission to add milestone.*
-	 */
-	readonly milestone?: string;
-
-	/**
-	 * The user to assign to the issue.
-	 *
-	 * *Requires the user to have the permission to add assignee.*
-	 */
-	readonly assignee?: string;
-
-	/**
-	 * The projects to add the issue to.
-	 * The project reference format is `user/<project-number>`, for example, if the URL to the project is `https://github.com/sindresorhus/some-repo/projects/3`, the project reference would be `some-repo/3`.
-	 *
-	 * *Requires the user to have the permission to add projects.*
-	 */
-	readonly projects?: string[];
+	You are required to either specify the `repoUrl` option or both the `user` and `repo` options.
+	*/
+	type Options = RepoUrlOptions | UserAndRepoOptions;
 }
 
-interface RepoUrlOptions extends CommonOptions {
+declare const newGithubIssueUrl: {
 	/**
-	 * The full URL to the repo.
-	 */
-	readonly repoUrl: string;
-}
+	Generate a URL for opening a new GitHub issue with prefilled title, body, and other fields.
 
-interface UserAndRepoOptions extends CommonOptions {
-	/**
-	 * GitHub username or organization.
-	 */
-	readonly user: string;
+	@example
+	```
+	import newGithubIssueUrl = require('new-github-issue-url');
+	import opn = require('opn');
 
-	/**
-	 * GitHub repo.
-	 */
-	readonly repo: string;
-}
+	const url = newGithubIssueUrl({
+		user: 'sindresorhus',
+		repo: 'new-github-issue-url',
+		body: '\n\n\n---\nI\'m a human. Please be nice.'
+	});
+	//=> 'https://github.com/sindresorhus/new-github-issue-url/issues/new?body=%0A%0A%0A---%0AI%27m+a+human.+Please+be+nice.'
 
-/**
- * You are required to either specify the `repoUrl` option or both the `user` and `repo` options.
- */
-export type Options = RepoUrlOptions | UserAndRepoOptions
+	// Then open it
+	opn(url);
+	```
+	*/
+	(options: newGithubIssueUrl.Options): string;
 
-/**
- * Generate a URL for opening a new GitHub issue with prefilled title, body, and other fields.
- *
- * @example
- *
- * import newGithubIssueUrl from 'new-github-issue-url';
- * import opn from 'opn';
- *
- * const url = newGithubIssueUrl({
- * 	user: 'sindresorhus',
- * 	repo: 'new-github-issue-url',
- * 	body: '\n\n\n---\nI\'m a human. Please be nice.'
- * });
- * //=> 'https://github.com/sindresorhus/new-github-issue-url/issues/new?body=%0A%0A%0A---%0AI%27m+a+human.+Please+be+nice.'
- *
- * // Then open it
- * opn(url);
- */
-export default function newGithubIssueUrl(options: Options): string;
+	// TODO: Remove this for the next major release, refactor the whole definition to:
+	// declare function newGithubIssueUrl(options: newGithubIssueUrl.Options): string;
+	// export = newGithubIssueUrl;
+	default: typeof newGithubIssueUrl;
+};
+
+export = newGithubIssueUrl;
--- index.test-d.ts
+++ index.test-d.ts
@@ -1,12 +1,16 @@
-import {expectType} from 'tsd-check';
-import newGithubIssueUrl from '.';
+import {expectType} from 'tsd';
+import newGithubIssueUrl = require('.');
 
-expectType<string>(newGithubIssueUrl({
-	repoUrl: 'https://github.com/sindresorhus/new-github-issue-url',
-	body: 'test'
-}));
+expectType<string>(
+	newGithubIssueUrl({
+		repoUrl: 'https://github.com/sindresorhus/new-github-issue-url',
+		body: 'test'
+	})
+);
 
-expectType<string>(newGithubIssueUrl({
-	user: 'sindresorhus',
-	repo: 'new-github-issue-url'
-}));
+expectType<string>(
+	newGithubIssueUrl({
+		user: 'sindresorhus',
+		repo: 'new-github-issue-url'
+	})
+);
